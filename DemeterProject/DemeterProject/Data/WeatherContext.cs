1bf5d518a96e38dd352c15c9ee117adcd7856e20
Ha Pham <selena.ngocha@gmail.com>
2022-05-13 22:16:27 +0700

    Layout ver 1

---
+++ DemeterProject/DemeterProject/Data/WeatherContext.cs
@@ -0,0 +1,21 @@
+﻿using DemeterProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemeterProject.Data
+{
+    public class WeatherContext : DbContext
+    {
+        public WeatherContext(DbContextOptions<WeatherContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Weather> Weathers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Location>().ToTable("Location");
+            modelBuilder.Entity<Weather>().ToTable("Weather");
+        }
+    }
+}
