7012b2f1f82a1290438e05a46822fc3a3686418c
Ha Pham <selena.ngocha@gmail.com>
2022-05-21 20:18:07 +0700

    Change proj folder and autocomplete

---
+++ DemeterWeather/DemeterWeather/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using DemeterWeather.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DemeterProject.Data;
+
+namespace DemeterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        db dbop = new db();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Index(string CountryName, string CountryID)
+        {
+            ViewBag.Message = "Country Name: " + CountryName + "Country ID: " + CountryID;
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult CountryList(string prefix)
+        {
+            List<LocationList> list = dbop.LocationGet(prefix);
+            return Json(list);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
