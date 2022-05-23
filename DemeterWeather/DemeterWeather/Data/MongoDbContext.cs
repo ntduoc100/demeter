839518e5ab3f5c629b06dd3839926754a4cbd572
Ha Pham <selena.ngocha@gmail.com>
2022-05-23 15:53:04 +0700

    autocomplete search with db

---
+++ DemeterWeather/DemeterWeather/Data/MongoDbContext.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Driver;
+using DemeterWeather.Models;
+
+namespace DemeterWeather.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _mongoDb;
+        public MongoDbContext()
+        {
+            var client = new MongoClient("mongodb+srv://root:12345ADMIN@cluster0.5qjhz.mongodb.net/?retryWrites=true&w=majority");
+            _mongoDb = client.GetDatabase("demeter");
+        }
+
+        public IMongoCollection<Region> Region
+        {
+            get
+            {
+                return _mongoDb.GetCollection<Region>("region_data");
+            }
+        }
+
+        public IMongoCollection<WeatherForecast> Realtime
+        {
+            get
+            {
+                return _mongoDb.GetCollection<WeatherForecast>("realtime_data");
+            }
+        }
+
+        public IMongoCollection<WeatherForecast> Predict
+        {
+            get
+            {
+                return _mongoDb.GetCollection<WeatherForecast>("predict_data");
+            }
+        }
+    }
+}
---
+++ DemeterWeather/DemeterWeather/Models/Region.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DemeterWeather.Models
+{
+    public class Region
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("Place")]
+        public string Place { get; set; }
+        [BsonElement("id")]
+        public int PlaceId { get; set; }
+        [BsonElement("coordinate")]
+        public double[] Coordinate { get; set; }
+    }
+}
